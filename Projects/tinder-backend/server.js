68c680cd02234e4f36f0a77f8ddc70b417d07bbf
DecryptLife <benson.thomas2602@gmail.com>
2021-09-04 23:11:46 +0530

    Backend completed

--- Projects/tinder-backend/server.js
+++ Projects/tinder-backend/server.js
@@ -1,8 +1,53 @@
+import e from "express";
 import express from "express";
 import mongoose from "mongoose";
+import Cors from "cors";
+import Cards from "./dbCards.js";
 
 // App config
+// creating instance
+const app = express();
+const port = process.env.PORT || 8001;
+const connection_url =
+  "mongodb+srv://admin:yctjzbeOLbTXh2bj@cluster0.0lige.mongodb.net/tinderdb?retryWrites=true&w=majority";
 
 // Middlewares
+app.use(express.json());
+app.use(Cors());
 
-// DB config
+// DB config (connection to DB happens here)
+mongoose.connect(connection_url, {
+  useNewUrlParser: true,
+  // useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+// API endpoints
+app.get("/", (req, res) => res.status(200).send("Hello Benson!"));
+
+app.post("/tinder/cards", (req, res) => {
+  const dbCard = req.body;
+
+  Cards.create(dbCard, (err, data) => {
+    if (err) {
+      // status(500) gives internal error
+      res.status(500).send(err);
+    } else {
+      // status(201) gives message created
+      res.status(201).send(data);
+    }
+  });
+});
+
+app.get("/tinder/cards", (req, res) => {
+  Cards.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
+
+// Listener
+app.listen(port, () => console.log(`listening on localhost: ${port}`));
