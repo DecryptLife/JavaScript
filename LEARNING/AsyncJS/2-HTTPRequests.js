1952ff1b1d564332879aee3c151980e2810482d7
DecryptLife <benson.thomas2602@gmail.com>
2021-09-10 22:01:26 +0530

    Promises

--- LEARNING/AsyncJS/2-HTTPRequests.js
+++ LEARNING/AsyncJS/2-HTTPRequests.js
@@ -1,43 +1,46 @@
 // we make http requests to get data from another server
 // we make these requests to API endpoints
 
-const getTodos = (resource, callback) => {
-  // creating request object
-  const request = new XMLHttpRequest();
+const getTodos = (resource) => {
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
 
-  // setting up the request
-  // first parameter is the type of request we want to make
-  // like - get, post, put etc
-  // second parameter is the end-point
-  // request.open("GET", "https://jsonplaceholder.typicode.com/todos/");
+    // setting up the request
+    // first parameter is the type of request we want to make
+    // like - get, post, put etc
+    // second parameter is the end-point
+    // request.open("GET", "https://jsonplaceholder.typicode.com/todos/");
 
-  // to send the request
-  // request.send();
+    // to send the request
+    // request.send();
 
-  // to track the request we can use event listeners readystatechange and
-  // is fired when there is a state change
+    // to track the request we can use event listeners readystatechange and
+    // is fired when there is a state change
 
-  request.addEventListener("readystatechange", () => {
-    // here the request will give some state values
-    // each has a particular meaning refer: https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/readyState
-    // 4 means the data has been downloaded
-    // status holds values ranging from 100-500 and they have various meanings, refer msdn website for detailed information
-    if (request.readyState == 4 && request.status === 200) {
-      // some errors could occur and still the state might reach 4 so this isn't the right way
-      // if there is an error then status would be 404 and the responseText would be {}
-      // so chceck for status as well error:404
-      // JSON.parse(json_text) converts the json to javascript objects
-      const data = JSON.parse(request.responseText);
-      callback(undefined, data);
-    } else if (request.readyState === 4) {
-      callback("could not fetch data", undefined);
-    }
+    request.addEventListener("readystatechange", () => {
+      // here the request will give some state values
+      // each has a particular meaning refer: https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/readyState
+      // 4 means the data has been downloaded
+      // status holds values ranging from 100-500 and they have various meanings, refer msdn website for detailed information
+      if (request.readyState == 4 && request.status === 200) {
+        // some errors could occur and still the state might reach 4 so this isn't the right way
+        // if there is an error then status would be 404 and the responseText would be {}
+        // so chceck for status as well error:404
+        // JSON.parse(json_text) converts the json to javascript objects
+        const data = JSON.parse(request.responseText);
+        resolve(data);
+      } else if (request.readyState === 4) {
+        reject("error getting resource");
+      }
+    });
+
+    //request.open("GET", "https://jsonplaceholder.typicode.com/todos/");
+    request.open("GET", resource);
+    request.send();
   });
-
-  //request.open("GET", "https://jsonplaceholder.typicode.com/todos/");
-  request.open("GET", resource);
-  request.send();
 };
+
+// creating request object
 
 console.log(1);
 console.log(2);
@@ -54,18 +57,57 @@ console.log(2);
 
 // callback-hell
 // here we can see that the code has started to look bad and if more files were there then it would be hard to maintain
-getTodos("todos/luigi.json", (err, data) => {
-  console.log(data);
-  getTodos("todos/mario.json", (err, data) => {
-    console.log(data);
-    getTodos("todos/shaun.json", (err, data) => {
-      console.log(data);
-    });
-  });
-});
+// getTodos("todos/luigi.json", (err, data) => {
+//   console.log(data);
+//   getTodos("todos/mario.json", (err, data) => {
+//     console.log(data);
+//     getTodos("todos/shaun.json", (err, data) => {
+//       console.log(data);
+//     });
+//   });
+// });
 
 // the solution to prevent the above callback hell is to use promises
 // promises allows to use the code one after the other
 
 console.log(3);
 console.log(4);
+
+// Promise
+// const getSomething = () => {
+//   // Promise has two types of return value (Either a reject or a resolve)
+//   return new Promise((resolve, reject) => {
+//     //fetch something
+//     // resolve("some data");
+//     // if any error
+//     reject("some error");
+//   });
+// };
+
+// Less neat
+// getSomething().then(
+//   (data) => {
+//     console.log(data);
+//   },
+//   (err) => {
+//     console.log(err);
+//   }
+// );
+
+// ***** Better way *****
+// getSomething()
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+// comes in handy when we have to chain promises
+getTodos("todos/luigi.json")
+  .then((data) => {
+    console.log("promise resolved: ", data);
+  })
+  .catch((err) => {
+    console.log("promise rejected: ", err);
+  });
